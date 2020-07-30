30b63c43f86d83bc0b02840d000dec3be22bfb44
Carlos Anriquez <canriquez@gmail.com>
2020-07-30 17:43:36 -0300

    add upper/lower case handling

--- src/cesarcipher.js
+++ src/cesarcipher.js
@@ -1,53 +1,68 @@
 const cesarCipher = () => {
-  const base = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
-    'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
-    'u', 'v', 'w', 'x', 'y', 'z'];
-    // 23
-  const cesarShift = (letter, offset) => {
-    for (let i = 0; i < base.length; i += 1) {
-      if (base[i] === letter) {
-        // if index + offset is bigger than length
-        if ((i + offset) > base.length - 1) {
-          const toEnd = (base.length - 1) - i; // offset = 3 : 25-23 = 2
-          return base[(offset - toEnd) - 1];
+    const base = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+        'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+        'u', 'v', 'w', 'x', 'y', 'z'];
+    // 
+
+    const upperCase = (char) => {
+        return char == char.toUpperCase()
+    }
+
+    const toCase = (char, upper) => {
+        if (upper) {
+            return char.toUpperCase();
+        } else {
+            return char;
         }
-        // if index - offset is less than 0
-        if ((i + offset) < 0) {
-          const toStart = (i); // offset = 3 : 25-23 = 2
-          const cIndex = base.length - ((offset + toStart) * -1);
-          return base[cIndex];
+    }
+
+    const cesarShift = (letter, offset, upper) => {
+        letter = letter.toLowerCase();
+        for (let i = 0; i < base.length; i += 1) {
+            if (base[i] === letter) {
+                // if index + offset is bigger than length
+                if ((i + offset) > base.length - 1) {
+                    const toEnd = (base.length - 1) - i; // offset = 3 : 25-23 = 2
+                    return toCase(base[(offset - toEnd) - 1], upper);
+                }
+                // if index - offset is less than 0
+                if ((i + offset) < 0) {
+                    const toStart = (i); // offset = 3 : 25-23 = 2
+                    const cIndex = base.length - ((offset + toStart) * -1);
+                    return toCase(base[cIndex], upper);
+                }
+
+                return toCase(base[i + offset], upper);
+            }
         }
-        return base[i + offset];
-      }
-    }
-    return letter;
-  };
+        return letter;
+    };
 
-  const encrypt = (message) => {
-    const word = message[0];
-    const offset = message[1];
-    let response = '';
-    for (let i = 0; i < word.length; i += 1) {
-      response += cesarShift(word[i], offset);
-    }
-    return response;
-  };
+    const encrypt = (message) => {
+        const word = message[0];
+        const offset = message[1];
+        let response = '';
+        for (let i = 0; i < word.length; i += 1) {
+            response += cesarShift(word[i], offset, upperCase(word[i]));
+        }
+        return response;
+    };
 
-  const decrypt = (message) => {
-    const word = message[0];
-    const offset = message[1] * -1;
-    let response = '';
-    for (let i = 0; i < word.length; i += 1) {
-      response += cesarShift(word[i], offset);
-    }
-    return response;
-  };
+    const decrypt = (message) => {
+        const word = message[0];
+        const offset = message[1] * -1;
+        let response = '';
+        for (let i = 0; i < word.length; i += 1) {
+            response += cesarShift(word[i], offset, upperCase(word[i]));
+        }
+        return response;
+    };
 
 
-  return {
-    encrypt,
-    decrypt,
-  };
+    return {
+        encrypt,
+        decrypt,
+    };
 };
 
 module.exports = cesarCipher;
--- src/cesarcipher.test.js
+++ src/cesarcipher.test.js
@@ -1,47 +1,47 @@
 const cesarCipher = require('./cesarcipher');
 
-const msg1 = ['a', 3];
+const msg1 = ['A', 3];
 const msg2 = ['x', 3];
 const msg3 = ['x', 9];
 const msg4 = ['c', -3];
 const msg5 = ['a', -5];
 const msg6 = ['.', -5];
-const msg7 = ['microverse, the best software school. ', -9];
-const msg8 = ['dztifmvijv, kyv svjk jfwknriv jtyffc. ', -9];
-const myCipher = cesarCipher();
+const msg7 = ['Microverse, The best Software School. ', -9];
+const msg8 = ['Dztifmvijv, Kyv svjk Jfwknriv Jtyffc. ', -9]
+let myCipher = cesarCipher();
 
 
 /* Cyphers a letter with positive shift */
 test('it encrypts a letter', () => {
-  expect(myCipher.encrypt(msg1)).toBe('d');
+    expect(myCipher.encrypt(msg1)).toBe('D');
 });
 
 test('it encrypts a letter', () => {
-  expect(myCipher.encrypt(msg2)).toBe('a');
+    expect(myCipher.encrypt(msg2)).toBe('a');
 });
 test('it encrypts a letter', () => {
-  expect(myCipher.encrypt(msg3)).toBe('g');
+    expect(myCipher.encrypt(msg3)).toBe('g');
 });
 
 /* Cyphers a letter with negative shift */
 test('it encrypts a letter', () => {
-  expect(myCipher.encrypt(msg4)).toBe('z');
+    expect(myCipher.encrypt(msg4)).toBe('z');
 });
 
 test('it encrypts a letter', () => {
-  expect(myCipher.encrypt(msg5)).toBe('v');
+    expect(myCipher.encrypt(msg5)).toBe('v');
 });
 
 test('it encrypts punctuation', () => {
-  expect(myCipher.encrypt(msg6)).toBe('.');
+    expect(myCipher.encrypt(msg6)).toBe('.');
 });
 
 /* Test a full case */
 test('it encrypts a full message', () => {
-  expect(myCipher.encrypt(msg7)).toBe('dztifmvijv, kyv svjk jfwknriv jtyffc. ');
+    expect(myCipher.encrypt(msg7)).toBe('Dztifmvijv, Kyv svjk Jfwknriv Jtyffc. ');
 });
 
-/* Decrypts */
+/*Decrypts */
 test('it encrypts a full message', () => {
-  expect(myCipher.decrypt(msg8)).toBe('microverse, the best software school. ');
+    expect(myCipher.decrypt(msg8)).toBe('Microverse, The best Software School. ');
 });
