6a4f36084977679575179c78b210c02c1ddde393
enelesmai <xochitl.selene.fp@gmail.com>
2020-07-30 11:03:05 -0500

    added more test to add and substract function

--- src/calculator.test.js
+++ src/calculator.test.js
@@ -2,6 +2,8 @@ const calculator = require('./calculator');
 
 const number1 = 10;
 const number2 = 5;
+const number3 = 3;
+const number4 = 6
 let myCalculator = calculator();
 
 test('it adds a number', () => {
@@ -10,4 +12,12 @@ test('it adds a number', () => {
 
 test('it substracts a number', () => {
     expect(myCalculator.substract(number1, number2)).toBe(5);
+});
+
+test('it adds a number', () => {
+    expect(myCalculator.add(number3, number4)).toBe(9);
+});
+
+test('it substracts a number', () => {
+    expect(myCalculator.substract(number3, number4)).toBe(-3);
 });
