d4913b879751025c8ef0c5ec6d7702ecfcf154ba
Carlos Anriquez <canriquez@gmail.com>
2020-07-30 13:17:01 -0300

    divide and multiply (methods and tests)

--- src/calculator.js
+++ src/calculator.js
@@ -2,10 +2,14 @@ const calculator = () => {
 
     const add = (number1, number2) => number1 + number2;
     const substract = (number1, number2) => number1 - number2;
+    const divide = (number1, number2) => number1 / number2;
+    const multiply = (number1, number2) => number1 * number2;
 
     return {
         add,
-        substract
+        substract,
+        divide,
+        multiply,
     };
 }
 
